fc0ac2634b2efb8340e89ce837328bd0d36a84a6
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-13 06:24:24 -0400

    exported a mongoose schema

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const mongoose = require('mongoose');
+
+
+//A schema defines document properties through an object where the keyname corresponds to the property name in the collection
+//here we defined aproperty called email with a schema type String
+let emailSchema = new mongoose.Schema({
+    email: String
+})
+
+module.exports = mongoose.model('Email', emailSchema);
+//We called the model constructor on the Mongoose instance and passed it the name of the collection & a referene to the schema
+
